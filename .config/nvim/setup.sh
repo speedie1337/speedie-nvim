7809f0c810d40381d19a9940fa13b519fd42006e
speedie <speedie@duck.com>
2022-12-19 16:10:43 +0100

    speedie-nvim | Add new config

--- .config/nvim/setup.sh
+++ .config/nvim/setup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 [ "$(whoami)" != "root" ] && printf "Run me as root.\n" && exit 1
 command -v npm > /dev/null && npm i -g bash-language-server || printf "npm not available, won't install bash-language-server\n"
-printf "Make sure ccls is installed.\n"
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # speedie-nvim
 
 speedie's neovim config
+
+run ./install.sh to install it, ./commit.sh to update
