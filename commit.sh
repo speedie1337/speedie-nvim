1d82d0795570ecfbbd1854482c405083ba1535c9
speedie <speedie@duck.com>
2023-03-04 20:04:47 +0100

    speedie-nvim | Add new config

--- commit.sh
+++ commit.sh
@@ -4,6 +4,7 @@ if [ -e "$HOME/.config/nvim" ]; then
     rm -rf .config/nvim
     mkdir -p .config
     cp -r $HOME/.config/nvim .config/
+    rm -rf .config/nvim/vimdata
 
     git add .config/* commit.sh install.sh
     git commit -a -m "speedie-nvim | Add new config"
