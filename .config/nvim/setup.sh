cd92310b79aef6752254f1ae65c3f4aef55f4dcc
speedie <speedie@speedie.site>
2023-12-27 00:18:07 +0100

    Slim down my Vim config, remove a bunch of bloat that I really don't need anymore.

--- .config/nvim/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-[ "$(whoami)" != "root" ] && printf "Run me as root.\n" && exit 1
-command -v npm > /dev/null && npm i -g bash-language-server || printf "npm not available, won't install bash-language-server\n"
