4fe1a89226e9552fed6762c5674aa2f4cd53e337
speedie <speedie@speedie.site>
2024-01-18 18:08:51 +0100

    speedie-nvim | Add new config

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ echo 3; sleep 1; echo 2; sleep 1; echo 1; sleep 1
 [ -e "$HOME/.config/nvim" ] && cp -r $HOME/.config/nvim /tmp/nvim-backup && rm -rf $HOME/.config/nvim
 
 cp -r .config/nvim $HOME/.config/
+mkdir -p $HOME/.config/nvim/spell
 
 echo "installed."
